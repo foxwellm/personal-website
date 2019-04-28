8c01b86a0fc2fe0ff98f5cc2593db300fdc7420e
Matthew Foxwell <foxwellmw@gmail.com>
2019-04-27 18:13:39 -0600

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Header/Header'
+import Display from '../Display/Display'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Display />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='Header'>
+      <div className='logo'></div>
+      <div className='circle-menu'></div>
+    </div>
+  )
+}
+
+export default Header
