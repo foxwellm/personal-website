bf451a916623e967f67e7bc5e8e692197dacda1e
Matthew Foxwell <foxwellmw@gmail.com>
2019-05-27 15:19:23 -0600

    Update Header compnent

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,14 @@ import { NavLink } from 'react-router-dom'
 const Header = () => {
   return (
     <div className='Header'>
-      <div className='logo'><NavLink to='/' className='nav-button' activeClassName='selected'>Home</NavLink></div>
+      <div className='logo'>
+        <NavLink to='/' className='logo-nav-button'>
+          MF</NavLink>
+          </div>
       <div className='circle-menu'>
-        <div className='menu-button contact'><NavLink to='/contact' className='nav-button' activeClassName='selected'>Contact</NavLink></div>
-        <div className='menu-button projects'><NavLink to='/projects' className='nav-button' activeClassName='selected'>Projects</NavLink></div>
         <div className='menu-button media'><NavLink to='/media' className='nav-button' activeClassName='selected'>Media</NavLink></div>
+        <div className='menu-button projects'><NavLink to='/projects' className='nav-button' activeClassName='selected'>Projects</NavLink></div>
+        <div className='menu-button contact'><NavLink to='/contact' className='nav-button' activeClassName='selected'>Contact</NavLink></div>
       </div>
     </div>
   )
