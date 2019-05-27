cf303b7244d694766d7618a49f1eefa66180bc0c
Matthew Foxwell <foxwellmw@gmail.com>
2019-05-27 15:17:12 -0600

    Update trapper keeper project

--- src/components/projectDisplay/TrapperKeeper/TrapperKeeper.js
+++ src/components/projectDisplay/TrapperKeeper/TrapperKeeper.js
@@ -1,29 +1,30 @@
 import React, { Component } from 'react';
-import trapper from '../../../assets/trapper.gif'
+import tMac from '../../../assets/trapperMac.png'
 
 
 export default class PicYourLandmark extends Component {
 
+  handleCardClick = () => {
+    this.props.changeProjNumber(3)
+  }
+
   render() {
+    const CardCss = ['Card', this.props.currentIndex !== 3 ? 'Card-not-active' : null].join(' ')
 
 
     return (
-
-      <article id="card-3" className="Card" >
-        <h1 className='card-header'>Trapper Keeper</h1>
-        {/* <div className='content-container'> */}
+      <article id="card-3" className={CardCss} onClick={this.handleCardClick} >
+        <h1 className={['card-header', this.props.currentIndex > 3 ? 'card-header-left' : null].join(' ')}>Trapper Keeper</h1>
         <div className='info-container'>
           <div className='demo-container'>
-            <img src={trapper} className='site' alt="This will display an animated GIF" />
+            <img src={tMac} className='site' alt="Trapper Keeper page" />
           </div>
           <div className='info'>
-            <h3>Description</h3>
-            <p className='info-p'>Pic Your Landmark is a React Native app based on the famous game 'Pokemon Go' - except our users can 'collect' famous landmarks! When a user visits a landmark, they can take and store their picture in our app. Users gain points/gems based on how many landmarks they have 'collected'. They can show off their status as well as collection of photos to their friends and family.</p>
-
-            <p>Available on <a href='https://play.google.com/store/apps/details?id=com.heatherandmatt.picyourlandmark' target='_blank'>Google Play </a></p>
-            <p>GitHub repo: <a href='https://github.com/foxwellm/PicYourLandmark' target='_blank'>github.com/foxwellm/PicYourLandmark</a></p>
+            <p className='info-p'>Similar to Google Keep - created with both front-end and back-end frameworks. You can save, edit, and delete personalized notes.</p>
+            <div className={['info-btn-container', this.props.currentIndex > 3 ? 'unclickable' : null].join(' ')}>
+              <a href='https://github.com/foxwellm/trapper-keeper' target='_blank' rel="noopener noreferrer" class="waves-effect waves-light #e53935 red darken-1 btn-large">GitHub</a>
+            </div>
           </div>
-
         </div>
       </article>
     )
