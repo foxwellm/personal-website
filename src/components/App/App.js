7942a8b276881b2ff0b7cad62fc7ac56c0d176b5
Matthew Foxwell <foxwellmw@gmail.com>
2019-05-27 15:20:53 -0600

    Update PicYourLandmark component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,7 +21,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Switch>
-          <Route exact path='/' component={Projects} />
+          <Route exact path='/' component={Home} />
           <Route exact path='/projects' component={Projects} />
           <Route exact path='/contact' component={Contact} />
           <Route exact path='/media' component={Media} />
--- src/components/projectDisplay/PicYourLandmark/PicYourLandmark.js
+++ src/components/projectDisplay/PicYourLandmark/PicYourLandmark.js
@@ -1,42 +1,33 @@
 import React, { Component } from 'react';
-import map from '../../../assets/Map.gif'
-// import takePhoto from '../../../assets/take-photo.gif'
-import login from '../../../assets/Login.gif'
-// import screen from '../../../assets/leaning3.png'
-import userprofile from '../../../assets/userprofile.gif'
+import map from '../../../assets/pMapMac.png'
+import login from '../../../assets/pLoginMac.png'
+
 
 
 export default class PicYourLandmark extends Component {
+  handleCardClick = () => {
+    this.props.changeProjNumber(0)
+  }
 
   render() {
-
+    const CardCss = ['Card', this.props.currentIndex !== 0 ? 'Card-not-active' : null].join(' ')
 
     return (
-
-      <article id="card-0" className="Card" >
-        <h1 className='card-header'>Pic Your Landmark</h1>
-        {/* <div className='content-container'> */}
+      <article id="card-0" className={CardCss} onClick={this.handleCardClick}>
+        <h1 className={['card-header', this.props.currentIndex > 0 ? 'card-header-left' : null].join(' ')}>Pic Your Landmark</h1>
         <div className='info-container'>
           <div className='demo-container'>
-            <img src={map} className='phone' alt="This will display an animated GIF" />
-            <img src={login} className='phone' alt="This will display an animated GIF" />
-            <img src={userprofile} className='phone' alt="This will display an animated GIF" />
+            <img src={map} className='phone phone-l' alt="Pic Your Landmark map page" />
+            <img src={login} className='phone phone-r' alt="Pic Your Landmark login page" />
           </div>
           <div className='info'>
-            <h3>Description</h3>
-            <p className='info-p'>Pic Your Landmark is a React Native app based on the famous game 'Pokemon Go' - except our users can 'collect' famous landmarks! When a user visits a landmark, they can take and store their picture in our app. Users gain points/gems based on how many landmarks they have 'collected'. They can show off their status as well as collection of photos to their friends and family.</p>
-
-            <p>Available on <a href='https://play.google.com/store/apps/details?id=com.heatherandmatt.picyourlandmark' target='_blank'>Google Play </a></p>
-            <p>GitHub repo: <a href='https://github.com/foxwellm/PicYourLandmark' target='_blank'>github.com/foxwellm/PicYourLandmark</a></p>
+            <p className='info-p'>Pic Your Landmark is a React Native app based on the famous game 'Pokemon Go' - except users can 'collect' famous landmarks! </p>
+            <div className={['info-btn-container', this.props.currentIndex > 0 ? 'unclickable' : null].join(' ')}>
+              <a href='https://play.google.com/store/apps/details?id=com.heatherandmatt.picyourlandmark' target='_blank' rel="noopener noreferrer" class="waves-effect waves-light #e53935 red darken-1 btn-large">Google Play</a>
+              <a href='https://github.com/foxwellm/PicYourLandmark' target='_blank' rel="noopener noreferrer" class="waves-effect waves-light #e53935 red darken-1 btn-large">GitHub</a>
+            </div>
           </div>
-
         </div>
-
-        {/* <img src={takePhoto} className='live' alt="This will display an animated GIF" /> */}
-        {/* </div> */}
-
-
-
       </article>
     )
   }
