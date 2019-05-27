355ace1739f523df9489cae93af2f95f4848a896
Matthew Foxwell <foxwellmw@gmail.com>
2019-05-27 15:18:45 -0600

    Update Media component

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -5,12 +5,12 @@ const Media = (props) => {
   return (
     <div className='Media'>
       <div className='media-container'>
-      {/* <div className='media-header-container'> */}
-
-      <h1 className='media-header'>Medium article on Linked Lists</h1>
-        <a className='media-header' href='https://medium.com/p/7f6b26207d93'>https://medium.com/p/7f6b26207d93</a>
-      {/* </div> */}
-        <img src={mediumGif} className='image' alt="This will display an animated GIF" />
+        <a href='https://medium.com/p/7f6b26207d93' target='_blank' rel="noopener noreferrer" >
+          <h1 className='media-header'>Medium article on Linked Lists</h1>
+        </a>
+        <a href='https://medium.com/p/7f6b26207d93' target='_blank' rel="noopener noreferrer" >
+          <img alt='Medium article gif of Linked Lists' href='https://medium.com/p/7f6b26207d93' target='_blank' rel="noopener noreferrer" src={mediumGif} className='image' />
+        </a>
       </div>
     </div>
   )
